46edb5ef78c42f505b85a638af7233e97cd7c305
Kenni Bawden <kenni.bawden@gmail.com>
2013-09-27 15:34:40 +1000

    Added some NLP processing to youtube comments

--- youtube.py
+++ youtube.py
@@ -4,9 +4,9 @@ import urllib2
 from lxml import objectify
 
 COMMENT_URL = "http://gdata.youtube.com/feeds/api/videos/{video_id}/comments?max-results={max_results}"
-VIDEO_ID = "lBzET3tBkmU"
+VIDEO_ID = "hbenhC7M8VI"
 RESULTS_PER_REQUEST = 50
-MAX_RESULTS = 66
+MAX_RESULTS = 1000
 
 # A class which stores the author and text of comments
 class Comment():
@@ -36,17 +36,42 @@ for index in xrange(0, MAX_RESULTS, RESULTS_PER_REQUEST):
   # Fetch the data
   result = urllib2.urlopen(url)
   # Parse the data
-  obj = objectify.fromstring(result.read())
+  obj = objectify.fromstring(result.read().replace('\xef\xbb\xbf', ''))
 
   # Place into comment objects
+  request_comments = []
   for child in obj.getchildren():
     if child.tag.endswith('entry'):
       content = child.content.text
       author = child.author.name.text
 
       comment = Comment(content, author)
-      comments.append(comment)
+      request_comments.append(comment)
+  
+  # Add the comments from this request to the pool of comments
+  comments += request_comments
 
-  print url
+  # Stop requests if all comments have been parsed
+  if len(request_comments) is 0:
+    break
 
-print comments
+
+# Do some NLP to find interesting results
+from collections import Counter
+import nltk
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_bag = Counter()
+for comment in comments:
+  if "should" in comment.text:
+    sentences = nltk.sent_tokenize(comment.text)
+    sent_words = [nltk.pos_tag(nltk.word_tokenize(comment.text)) for sentence in sentences if "should" in sentence]
+    sent_keywords = [
+      [word for word, pos in sentence
+        if word.lower() not in stopwords
+          and ("NN" in pos or pos is "VB")]
+      for sentence in sent_words]
+    keywords = sum(sent_keywords, [])
+    word_bag.update(keywords)
+
+print word_bag
